1072af4ab4852514482decea109b68ec0c579435
Christian Roy <christian.50.roy@gmail.com>
2018-01-27 10:08:59 -0700

    Start GameManager and ItemData SO

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameData gameData;
+    public int[] score;
+    public float timeLimit;
+
+    public static GameManager instance;
+
+    public void Awake()
+    {
+        score = new int[gameData.numberOfPlayers];
+        timeLimit = gameData.timeLimit;
+
+        if (instance != null)
+        {
+            Debug.LogError("Duplicate Game Managers Found.");
+            Destroy(gameObject);
+        }
+        instance = this;
+    }
+
+    public void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+        TimerTick();   
+    }
+
+    private void TimerTick()
+    {
+        timeLimit -= Time.deltaTime;
+
+        if (timeLimit <= 0)
+        {
+            EndGame();
+        }
+    }
+
+    private void EndGame()
+    {
+
+    }
+
+    public void BeamItem()
+    {
+
+    }
+}
---
+++ Assets/_Scripts/ItemData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ItemData : ScriptableObject
+{
+    public Mesh itemMesh;
+
+    public new string name;
+    public int pointValue;    
+}
