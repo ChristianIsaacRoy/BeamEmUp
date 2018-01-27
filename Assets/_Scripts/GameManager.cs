f54ed8099d043d941aac6fadd890128350a20faa
Christian Roy <christian.50.roy@gmail.com>
2018-01-27 10:44:02 -0700

    Add Game Events, manager instantiation

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameData gameData;
-    public int[] score;
-    public float timeLimit;
+
+    public GameEvent onPlayerScored;
+
+    private int[] playerScores;
+    private float timeLimit;
+    private List<ItemData>[] playerItems;
 
     public static GameManager instance;
 
+    #region Monobehaviours
     public void Awake()
     {
-        score = new int[gameData.numberOfPlayers];
-        timeLimit = gameData.timeLimit;
-
         if (instance != null)
         {
             Debug.LogError("Duplicate Game Managers Found.");
             Destroy(gameObject);
         }
         instance = this;
+
+        InstantiateManager();
     }
 
     public void Start()
@@ -31,6 +35,24 @@ public class GameManager : MonoBehaviour
     public void Update()
     {
         TimerTick();   
+    }
+    #endregion
+
+    private void InstantiateManager()
+    {
+        // Create arrays
+        playerScores = new int[gameData.numberOfPlayers];
+        playerItems = new List<ItemData>[gameData.numberOfPlayers];
+
+        // Fill up arrays
+        for (int i = 0; i < gameData.numberOfPlayers; i++)
+        {
+            playerItems[i] = new List<ItemData>();
+            playerScores[i] = 0;
+        }
+
+        // Starting variable values
+        timeLimit = gameData.timeLimit;
     }
 
     private void TimerTick()
@@ -48,8 +70,10 @@ public class GameManager : MonoBehaviour
 
     }
 
-    public void BeamItem()
+    public void AddItemToPlayer(int playerID, ItemData data)
     {
-
+        playerItems[playerID].Add(data);
+        playerScores[0] += data.pointValue;
+        onPlayerScored.Raise();
     }
 }
