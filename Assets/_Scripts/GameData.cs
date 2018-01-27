462d246e79478e2da575b555f963f77a25230d7a
Christian Roy <christian.50.roy@gmail.com>
2018-01-26 20:41:09 -0700

    Add GameData SO

---
+++ Assets/_Scripts/GameData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class GameData : ScriptableObject
+{
+    public int numberOfPlayers = 2;
+    public int timeLimit = 30;
+}
