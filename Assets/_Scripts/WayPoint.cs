f64ba009134444c00906d4b6e5632e6b6faa1281
Colton Lee <coltonlee4407@hotmail.com>
2018-01-27 15:13:10 -0700

    Fixes to prop spawner

---
+++ Assets/_Scripts/WayPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoint : MonoBehaviour {
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, .5f);
+    }
+}
