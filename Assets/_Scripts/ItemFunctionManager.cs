e48351d18ec4d559311a8ba06a8dbff6906d61bb
Papapopp <32002023+Papapopp@users.noreply.github.com>
2018-01-27 11:32:47 -0700

    floating game object

---
+++ Assets/_Scripts/ItemFunctionManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemFunctionManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void itemPickup()
+    {
+        //makes item dissapear (get zapped and pulled in)
+
+        Destroy(this);
+    }
+ 
+	
+	// Update is called once per frame
+	void Update () {
+        //item rotation
+        transform.Rotate(0, 10 * Time.deltaTime, 0);
+        //item float
+        transform.Translate(0, Mathf.Sin(Time.fixedTime)/60, 0);
+	}
+}
