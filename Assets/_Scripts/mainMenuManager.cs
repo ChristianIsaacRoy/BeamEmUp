8f262ff8672b1e2cfbace4adc6e2140a1069c3b5
Papapopp <32002023+Papapopp@users.noreply.github.com>
2018-01-26 23:38:01 -0700

    menus
    the infrastucture and objects for the pause and main menus

---
+++ Assets/_Scripts/mainMenuManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainMenuManager : MonoBehaviour {
+
+    public GameObject startMenu;
+    public GameObject PlayerCountChoiceMenu;
+
+    // Use this for initialization
+    void Start () {
+        startMenu.SetActive(true);
+        PlayerCountChoiceMenu.SetActive(false);
+    }
+
+    public void startPressed ()
+    {
+        startMenu.SetActive(false);
+        PlayerCountChoiceMenu.SetActive(true);
+    }
+	
+    public void exitPressed ()
+    {
+        Application.Quit();
+    }
+
+    public void startGame (int Mode)
+    {
+        //Modes 1-4 correlate to 1-4 player games
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
