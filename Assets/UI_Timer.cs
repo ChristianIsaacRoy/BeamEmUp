dc63b0f9bc7f7824f0a913c082b2818c95867b30
Christian Roy <christian.50.roy@gmail.com>
2018-01-27 12:18:08 -0700

    Add prefabs, UI for timers and scores,

---
+++ Assets/UI_Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Timer : MonoBehaviour
+{
+    public Text timerText;
+
+    private GameManager gm;
+
+    public void Start()
+    {
+        gm = GameManager.instance;
+    }
+
+    public void Update()
+    {
+        int seconds = (int)(gm.GetTimeLeft() % 60);
+        int minutes = (int)(gm.GetTimeLeft() / 60);
+        string time = minutes + ":" + seconds;
+
+        timerText.text = time;
+    }
+}
